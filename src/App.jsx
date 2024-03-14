e5f34a1e84bc8735924af8fe35a1db59736439f6
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-03-15 00:11:23 +0200

    Changes components

--- src/App.jsx
+++ src/App.jsx
@@ -9,85 +9,68 @@ import css from './App.module.css';
 import "modern-normalize";
 import { Toaster } from 'react-hot-toast';
 import { useEffect, useState } from 'react';
-import { requestPhoto, requestPhotoSearch } from './services/api';
+import { requestPhotos, requestPhotosByQuery } from './services/api';
 
 function App() {
   const [photos, setPhotos] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [page, setPage] = useState(1); 
+  const [searchQuery, setSearchQuery] = useState(null);
 
- const handleLoadMore = async () => { 
-    try {
-    setIsLoading(true);
-    const data = await requestPhotoSearch(searchQuery, page + 1);
-    setPhotos([...photos, ...data.results]);
-    setPage(prevPage => prevPage + 1);
-    } catch(error) {
-      setIsError(true);
-    } finally {
-      setIsLoading(false)
+  useEffect(() => { 
+    async function fetchFotos() {
+      
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const data = await requestPhotos();
+        setPhotos(data.photos);
+
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  };
 
-  useEffect(() => {   
-   async function fetchPhotos () {
-     try {
-      setIsLoading(true);   
-       setIsError(false); 
-        const data = await requestPhoto(searchQuery, 1);
-         setPhotos(data);
-         setPage(2);
+    fetchFotos()
+  }, [])
 
-      } catch(error) {
-        setIsError(true)
+ const onSearchQuery = (query) => {
+    setSearchQuery(query);
+ };
 
+  useEffect(() => {
+    if (searchQuery === null) return;
+
+  async function fetchFotosByQuery() {
+      
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const data = await requestPhotosByQuery(searchQuery);
+        setPhotos(data);
+
+      } catch (error) {
+        setIsError(true);
       } finally {
-        setIsLoading(false); 
+        setIsLoading(false);
       }
-    }      
-        fetchPhotos();
-    }, [searchQuery]);
+    }
 
+    fetchFotosByQuery();
 
-    const onSearchQuery = (query) => {
-      setSearchQuery(query);
-      setPage(1);
-    };
+  }, [searchQuery]);
 
-
-  useEffect(() => {  
-    if(searchQuery === null) return;
-
-    async function fetchPhotos() {
-     try {
-      setIsLoading(true);   
-       setIsError(false); 
-        const data = await requestPhotoSearch(searchQuery, page);
-         setPhotos(data.results);           
-         setPage(prevPage => prevPage + 1);
-
-      } catch(error) {
-        setIsError(true)
-
-      } finally {
-        setIsLoading(false); 
-      }
-    }      
-        fetchPhotos();
-    }, [searchQuery, page]);
-  
- 
   return (
     <div className={css.container}>
-    <SearchBar onSubmit={onSearchQuery}/>
+    <SearchBar onSubmit={onSearchQuery} /> 
     <Toaster />
     <ImageGallery photos={photos} />
-    {isLoading && <Loader/>}
+    {isLoading && <Loader/>} 
     {isError && <ErrorMessage/>}
-    {photos && photos.length > 0 && <LoadMoreBtn onClick={handleLoadMore}/>}
-    <ImageModal/>
+    {photos && photos.length > 0 && <LoadMoreBtn />} 
+     <ImageModal/>
     </div>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,21 @@
 import axios from "axios";
 
 const ACCESS_KEY = "YQ_yL-LQzjl6Sr0ymPf0zmir3fmTWR81QSVXvomwQP4";
-export const requestPhoto = async () => {
-    const response = await axios.get('https://api.unsplash.com/photos', {
-        headers: {
-            Authorization: `Client-ID ${ACCESS_KEY}`
-        }
-    });
-    return response.data;
-}
 
-export const requestPhotoSearch = async (searchQuery, page = 1, perPage = 12) => {
-    const response = await axios.get(`https://api.unsplash.com/search/photos?&query=${searchQuery}`, {
+export const requestPhotos = async () => {
+    const { data } = await axios.get(`https://api.unsplash.com/photos`, {
         headers: {
             Authorization: `Client-ID ${ACCESS_KEY}`
         },
-        params: {
-            query: searchQuery,
-            page,
-            per_page: perPage
-        }
     });
-    return response.data;
+    return data;
+};
+
+export const requestPhotosByQuery = async (query) => {
+       const { data } = await axios.get(`https://api.unsplash.com/search/photos?query=${query}`, {
+          headers: {
+            Authorization: `Client-ID ${ACCESS_KEY}`
+          },
+    });
+    return data;
 }
